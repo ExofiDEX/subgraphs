20f8038e3eb7d90542cd52abf475834803062e7e
Ing. Michael Goldfinger <mg@cobra.io>
2022-07-28 23:07:02 +0200

    feat: add MFG subgraph

--- config/goerli.js
+++ config/goerli.js
@@ -5,5 +5,10 @@ module.exports =
 	{
 		address: "0x5865A76A135a1fBE0f4f9072F0dEcd9F5813570c",
 		startBlock: 7207907
+	},
+	magneticFieldGenerator:
+	{
+		address: "0x9786548718Ef3F7E7F975a550e997098e3A9E617",
+		startBlock: 7249688
 	}
 }
